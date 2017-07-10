88903af92176b6009bdd0d5b899066e6c5c8e671
Mickey William Fischer <mwf@swordfischer.com>
2017-07-11 00:31:49 +0200

    use keyrune and magicthegathering.io sdk instead

--- getwizards.py
+++ getwizards.py
@@ -1,12 +1,13 @@
-from BeautifulSoup import BeautifulSoup
-import urllib2
-import re
+from mtgsdk import Set
+import calendar
+from time import strptime
 
 html = '''
 <!DOCTYPE html>
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link href="//cdn.jsdelivr.net/npm/keyrune@latest/css/keyrune.css" rel="stylesheet" type="text/css" />
     <link rel='stylesheet' href='style.css'>
   </head>
   <body>
@@ -15,60 +16,39 @@ html = '''
 </html>
 '''
 
-
-url = urllib2.urlopen(
-    'http://magic.wizards.com/en/game-info/products/card-set-archive')
-
-content = url.read()
-
-soup = BeautifulSoup(content)
+sets = Set.all()
+sets.sort(key=lambda x: x.release_date, reverse=True)
 
 mtg_list = []
 
-divs = soup.findAll("div", { "class" : "card-set-archive-table" })
-
-for div in divs:
-    if div.parent.get('id') != 'magic-online-products':
-        for li in div.findAll("li"):
-            if li['class'] != 'title':
-                try:
-                    mtg_icon = li.find("span", {"class" : "icon"}).img['src']
-                except (NameError, TypeError):
-                    mtg_icon = 'none.png'
-                try:
-                    mtg_set = li.find("span", {"class" : "nameSet"}).text
-                except (NameError, TypeError):
-                    mtg_set = 'N/A'
-                try:
-                    mtg_date = li.find("span", {"class" : "releaseDate"}).text
-                except (NameError, TypeError):
-                    mtg_date = 'N/A'
-                if re.match('World Champ', mtg_set) is None:
-                    mtg_list.append({
-                        'icon': mtg_icon,
-                        'set': mtg_set,
-                        'date':mtg_date
-                    })
+for set in sets:
+    if set.online_only != 1:
+        mtg_list.append({
+            'code': set.code.lower(),
+            'name': set.name,
+            'month': calendar.month_name[strptime(set.release_date, '%Y-%m-%d').tm_mon],
+            'year': strptime(set.release_date, '%Y-%m-%d').tm_year
+        })
 
 items = []
+
 for item in mtg_list:
     items.append('''
     <div class='container'>
-      <div class='symbol'>
-        <img src='{icon}'>
-      </div>
-      <div class='fullinfo'>
-        <div class='set'><p>{set}</p></div>
-        <div class='date'><p>{date}</p></div>
-      </div>
+        <div class='symbol'>
+            <i class='ss ss-{code}'></i>
+        </div>
+        <div class='fullinfo'>
+            <div class='set'><p>{name}</p></div>
+            <div class='date'><p>{month}, {year}</p></div>
+        </div>
     </div>
     <div class='ruler'>
     </div>
     '''.format(**item))
 
-
 items = '\n'.join(items)
 with open('mtg.html', 'w') as text_file:
     text_file.write(html.format(items))
-
 text_file.close()
+
